9ebdd0c1117619b62989529a5269d494138dbdf7
JEM1F02 <00001159138667sp@al.educacao.sp.gov.br>
2024-08-16 10:49:15 -0300

    minha primeira programação

---
+++ skat.js
@@ -0,0 +1,17 @@
+let cor;
+let posicaoHorizontal; 
+let posicaoVertical; 
+
+let posicaoHorizontal2; 
+let posicaoVertical2; 
+
+function setup() {
+  createCanvas(400, 400);
+   background(color(100,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 0;
+  posicaoVertical = 200;
+  
+  posicaoHorizontal2 = 0;
+  posicaoVertical2 = 200;
+}
